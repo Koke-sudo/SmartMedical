35afa7e20f6bc269f283360387ee97a7007e12c8
tangyuhang0829 <m15731495370@163.com>
2021-04-19 11:21:57 +0800

    添加项目文件。

---
+++ SmartMedical/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartMedical.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
